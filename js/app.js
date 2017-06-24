de2faaca624b561ea02b45136b30a03348fcf223
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-06-23 18:51:21 -0700

    Add cat levels to cat clicker

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 var ViewModel = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
+    this.levels = ko.computed(function() {
+        if (this.clickCount() < 10) {
+            return 'NewBorn';
+        } else if (this.clickCount() < 20) {
+            return 'Infant';
+        } else {
+            return 'Teen';
+        }
+    }, this);
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
 
